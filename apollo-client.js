984655b08ba77c25a1b5a515754cccb78fa42c88
Anvarmirzo <www.latvienhoasadrizeges@gmail.com>
2022-05-25 22:45:07 +0500

    stepzen

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-	uri: 'http://localhost:5001/api/kneeling-opossum',
+	uri: 'https://kabul.stepzen.net/api/kneeling-opossum/__graphql',
 	headers: {
 		Authorization: `Apikey ${process.env.NEXT_PUBLIC_STEPZEN_KEY}`,
 	},
